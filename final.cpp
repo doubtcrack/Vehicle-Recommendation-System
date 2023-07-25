a0f130d0dbb2085dd67adc11c31a79798f2c3d5b
doubtcrack <1.edujock@gmail.com>
2023-07-25 13:40:06 +0530

    Optimised nearest search

---
+++ final.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// this function finds Distance; takes location ID as loc1 and loc2
+double calculateDistance(int loc1, int loc2) {
+    return abs(loc1-loc2); //It doesn't matter if we square it or not, as We just want minimum 3 distances out of all.
+}
+
+//Step 1 : Stores all the information about the vehicle.
+struct Vehicle {
+    string vehicleNo;
+    string category;
+    int passengerCapacity;
+    bool available;
+    string driverContactNo;
+};
+
+
+//Step-2 : Function that will return the vehicles
+// This function will return the 3 vehicles as per the problem.
+void getRecommendations(int inputLocationId, int numOfPassengers, unordered_map<int, Vehicle>& vehicles) {
+    
+    //Using priority queue, Because It will save our time to find nearest vehicles. We can use 2d vector to store all the distance and then sort it.
+    priority_queue<pair<int,int>> distances;
+    
+    
+    for (auto& vehiclePair : vehicles) {
+        int locationId = vehiclePair.first;
+        Vehicle& vehicle = vehiclePair.second;
+
+        //If vehichle is not present or It has not required capacity then we should recommend the vehicle
+        if (vehicle.available && vehicle.passengerCapacity >= numOfPassengers) {  
+            double distance = calculateDistance(inputLocationId, locationId);
+            distances.push(make_pair(distance, locationId));
+
+            if(distances.size()>3){   //For optimising the space; we will need only 3 nearest vehicles
+                distances.pop();
+            }
+
+        }
+    }
+
+    while(!distances.empty()){
+        int nearestVehicle = distances.top().second;
+        distances.pop();
+        Vehicle v1 = vehicles.at(nearestVehicle);
+        cout << v1.vehicleNo << ", " << v1.category << ", " << v1.driverContactNo << endl; //printing the results inside the function as mentioned in problem statement
+    }
+}
+
+int main() 
+{
+
+    unordered_map<int, Vehicle> vehicles = {
+        {100, {"HR-26 0091", "car", 4, true, "090909878"}},
+        {201, {"HR-26 7896", "auto", 2, true, "09876534"}},
+        {301, {"HR-26 2345", "car", 5, true, "78902564"}},
+        {011, {"HR-26 8798", "car", 4, false, "987654"}},
+        {101, {"HR-26 6754", "auto", 2, false, "78990245"}},
+        {110, {"HR-26 7891", "bike", 1, true, "87902453"}},
+        {102, {"HR-26 5678", "bike", 1, false, "78902453"}},
+    };
+
+    // Taking Input
+    int inputLocationId, numOfPassengers;
+    cout << "Enter your location ID and number of passengers: ";
+    cin >> inputLocationId >> numOfPassengers;
+
+    // As mentioned, the function which will recommend and print the results.
+    getRecommendations(inputLocationId, numOfPassengers, vehicles);
+    return 0;
+}
--- script.js
+++ script.js
@@ -1,112 +1,103 @@
-// Vehicle data
-const vehicles = [
-  {
-    locationId: 100,
-    vehicleNo: "HR-26 0091",
-    category: "car",
-    numPassengers: 4,
-    available: true,
-    driverContactNo: 90909878,
-  },
-  {
-    locationId: 201,
-    vehicleNo: "HR-26 7896",
-    category: "auto",
-    numPassengers: 2,
-    available: true,
-    driverContactNo: 9876534,
-  },
-  {
-    locationId: 301,
-    vehicleNo: "HR-26 2345",
-    category: "car",
-    numPassengers: 5,
-    available: true,
-    driverContactNo: 78902564,
-  },
-  {
-    locationId: 0o11,
-    vehicleNo: "HR-26 8798",
-    category: "car",
-    numPassengers: 4,
-    available: false,
-    driverContactNo: 987654,
-  },
-  {
-    locationId: 101,
-    vehicleNo: "HR-26 6754",
-    category: "auto",
-    numPassengers: 2,
-    available: false,
-    driverContactNo: 78990245,
-  },
-  {
-    locationId: 110,
-    vehicleNo: "HR-26 7891",
-    category: "bike",
-    numPassengers: 1,
-    available: true,
-    driverContactNo: 87902453,
-  },
-  {
-    locationId: 102,
-    vehicleNo: "HR-26 5678",
-    category: "bike",
-    numPassengers: 1,
-    available: false,
-    driverContactNo: 78902453,
-  },
-];
-
-// Calculate Euclidean distance
-function calculateDistance(loc1, loc2) {
-  return Math.pow(loc1 - loc2, 2);
-}
-
-// Recommend nearest vehicles based on user input
-function recommendNearestVehicles(userLocation, numPassengers) {
-  const availableVehicles = vehicles.filter(
-    (vehicle) => vehicle.available && vehicle.numPassengers >= numPassengers
+document.getElementById("recommendBtn").addEventListener("click", function () {
+  const inputLocationId = parseInt(
+    document.getElementById("userLocation").value
   );
-
-  availableVehicles.sort(
-    (a, b) =>
-      calculateDistance(userLocation, a.locationId) -
-      calculateDistance(userLocation, b.locationId)
-  );
-
-  return availableVehicles.slice(0, 3);
-}
-
-// Display the recommended vehicles in the UI
-function showRecommendations() {
-  const userLocation = parseInt(document.getElementById("userLocation").value);
-  const numPassengers = parseInt(
+  const numOfPassengers = parseInt(
     document.getElementById("numPassengers").value
   );
-  const vehicleList = document.getElementById("vehicleList");
-  const outputDiv = document.getElementById("output");
 
-  const recommendedVehicles = recommendNearestVehicles(
-    userLocation,
-    numPassengers
-  );
+  // For simplicity, let's assume we have the vehicle data in a JavaScript object as follows:
 
-  // Clear previous recommendations
-  vehicleList.innerHTML = "";
+  const vehicles = {
+    100: {
+      vehicleNo: "HR-26 0091",
+      category: "car",
+      passengerCapacity: 4,
+      available: true,
+      driverContactNo: "090909878",
+    },
+    201: {
+      vehicleNo: "HR-26 7896",
+      category: "auto",
+      passengerCapacity: 2,
+      available: true,
+      driverContactNo: "09876534",
+    },
+    301: {
+      vehicleNo: "HR-26 2345",
+      category: "car",
+      passengerCapacity: 5,
+      available: true,
+      driverContactNo: "78902564",
+    },
+    0o11: {
+      vehicleNo: "HR-26 8798",
+      category: "car",
+      passengerCapacity: 4,
+      available: false,
+      driverContactNo: "987654",
+    },
+    101: {
+      vehicleNo: "HR-26 6754",
+      category: "auto",
+      passengerCapacity: 2,
+      available: false,
+      driverContactNo: "78990245",
+    },
+    110: {
+      vehicleNo: "HR-26 7891",
+      category: "bike",
+      passengerCapacity: 1,
+      available: true,
+      driverContactNo: "87902453",
+    },
+    102: {
+      vehicleNo: "HR-26 5678",
+      category: "bike",
+      passengerCapacity: 1,
+      available: false,
+      driverContactNo: "78902453",
+    },
+  };
 
-  if (recommendedVehicles.length > 0) {
-    outputDiv.classList.remove("hidden");
-    for (const vehicle of recommendedVehicles) {
+  // Function to calculate distance
+  function calculateDistance(loc1, loc2) {
+    return Math.abs(loc1 - loc2);
+  }
+
+  // Function to recommend the nearest vehicles
+  function getRecommendations(inputLocationId, numOfPassengers, vehicles) {
+    const distances = [];
+
+    for (const [locationId, vehicle] of Object.entries(vehicles)) {
+      if (vehicle.available && vehicle.passengerCapacity >= numOfPassengers) {
+        const distance = calculateDistance(
+          inputLocationId,
+          parseInt(locationId)
+        );
+        distances.push({ distance, vehicle });
+      }
+    }
+
+    distances.sort((a, b) => a.distance - b.distance);
+
+    const recommendedVehicles = distances.slice(0, 3);
+
+    const vehicleList = document.getElementById("vehicleList");
+    vehicleList.innerHTML = "";
+    recommendedVehicles.forEach(({ vehicle }) => {
       const listItem = document.createElement("li");
       listItem.textContent = `${vehicle.vehicleNo}, ${vehicle.category}, ${vehicle.driverContactNo}`;
       vehicleList.appendChild(listItem);
-    }
-  } else {
-    outputDiv.classList.add("hidden");
-  }
-}
+    });
 
-document
-  .getElementById("recommendBtn")
-  .addEventListener("click", showRecommendations);
+    const outputDiv = document.getElementById("output");
+    if (recommendedVehicles.length > 0) {
+      outputDiv.classList.remove("hidden");
+    } else {
+      outputDiv.classList.add("hidden");
+    }
+  }
+
+  getRecommendations(inputLocationId, numOfPassengers, vehicles);
+});
